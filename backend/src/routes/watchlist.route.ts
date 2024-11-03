3dea84b5bdfc5e4a34d7c565938f23461d103b7b
Anurag Khobragae <anuragk2204@gmail.com>
2024-11-03 21:39:29 +0530

    complete backed

--- backend/src/routes/watchlist.route.ts
+++ backend/src/routes/watchlist.route.ts
@@ -1,11 +1,14 @@
 import express from "express";
-import {second} from "../controllers/watchlist.controller.js"
+import {add, remove, list} from "../controllers/watchlist.controller.js"
+import protectRoutes from "../middlewares/protectRoutes.js";
 
 
 const router = express.Router();
 
 
 
-router.get('/second', second)
+router.post('/add',protectRoutes, add);
+router.put('/remove',protectRoutes,  remove);
+router.get('/list',protectRoutes, list);
 
 export default router;
